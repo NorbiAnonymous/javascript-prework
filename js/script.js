155859ff8a59d75ccfab5fe7ccbc6966fc44d7a3
Norbert Kloc <norb906@gmail.com>
2025-03-30 15:00:13 +0200

    dodanie ruchu gracza

--- js/script.js
+++ js/script.js
@@ -85,6 +85,9 @@
 // printMessage('<br>');
 // //4.Ćwiczenie Zapisywanie odpowiedzi gracza
 
+// Zadanie: Dokończenie logiki gry
+
+// Odczytanie ruchu komputera
 let computerInput = Math.floor(Math.random() * 3) + 1;
 let computerMove = 'nieznany ruch';
 
@@ -98,3 +101,18 @@ if (computerInput === 1) {
 
 console.log('Ruch Komputera to: ' + computerMove);
 
+// Odczytanie ruchu gracza ma on 3 możliwości 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput === '1') {
+	playerMove = 'kamień';
+} else if (playerInput === '2') {
+	playerMove = 'papier';
+} else if (playerInput === '3') {
+	playerMove = 'nożyce';
+}
+
+console.log('Twój ruch to: ' + playerMove);
